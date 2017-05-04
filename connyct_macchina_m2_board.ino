a53f395e09ea264e54602f61d88b3462ae74778b
Connyct <sushant.mainaly@connyct.com>
2017-05-04 01:20:26 -0500

    Add files via upload
    Initial Upload.

---
+++ connyct_macchina_m2_board.ino
@@ -0,0 +1,101 @@
+#include <OBD2.h>
+#include <DueTimer.h>
+#include "SamNonDuePin.h"
+
+/********************************************************************
+This example is built upon the CANAcquisition class and the OBDParmameter class using 11bit (non-extended) OBD2 ID's
+This example shows how to set up periodic data acquisition of OBD2 paramters based upon
+standard PID's. If you'd like to add another paramter,simply copy one of the definitions and modify it accordingly. 
+You may also need to add a new PID to the "OBD_PID" enum in the header file. 
+/********************************************************************/
+
+//create the CANport acqisition schedulers
+cAcquireCAN CANport0(CAN_PORT_0);
+
+/***** DEFINITIONS FOR OBD MESSAGES ON CAN PORT 0, see https://en.wikipedia.org/wiki/OBD-II_PIDs to add your own ***************/
+//char _name[10], char _units[10], OBD_PID pid,  uint8_t OBD_PID_SIZE size, bool _signed, OBD_MODE_REQ mode, float32 slope, float32 offset, cAcquireCAN *, extended ID;
+
+  cOBDParameter OBD_Speed(      "Speed "        , " KPH"    ,  SPEED       , _8BITS,   false,   CURRENT,  1,      0,  &CANport0, false);
+  cOBDParameter OBD_EngineSpeed("Engine Speed " , " RPM"    ,  ENGINE_RPM  , _16BITS,  false,   CURRENT,  0.25,   0,  &CANport0, false);
+  cOBDParameter OBD_Throttle(   "Throttle "     , " %"      ,  THROTTLE_POS, _8BITS,   false,   CURRENT,  0.3922, 0,  &CANport0, false);
+  cOBDParameter OBD_Coolant(    "Coolant "      , " C"      ,  COOLANT_TEMP, _8BITS,   false ,  CURRENT,  1,    -40,  &CANport0, false);
+  cOBDParameter OBD_EngineLoad( "Load "         , " %"      ,  ENGINE_LOAD , _8BITS,   false,   CURRENT,  0.3922, 0,  &CANport0, false);
+  cOBDParameter OBD_MAF(        "MAF "          , " grams/s"    ,  ENGINE_MAF  , _16BITS,  false,   CURRENT,  0.01,   0,  &CANport0, false);
+  cOBDParameter OBD_IAT(        "IAT "          , " C"      ,  ENGINE_IAT  , _8BITS,   false ,  CURRENT,  1,    -40,  &CANport0, false);
+  cOBDParameter OBD_FuelLevel("Fuel Level "     , " Percent",  FUEL_LEVEL        , _8BITS,   false,   CURRENT,  1,      0,  &CANport0, false);
+
+//#define Serial SerialUSB
+const int LOWPOW = PIN_EMAC_ERX0;
+
+void setup()
+{
+  pinModeNonDue(LOWPOW, OUTPUT);
+  digitalWriteNonDue(LOWPOW, HIGH);  
+  pinMode(28, OUTPUT); 
+  digitalWrite(28, LOW);    
+  
+
+  Serial.begin(115200);
+
+  //debugging message for monitor to indicate CPU resets are occuring
+  //Serial.println("System Reset");
+
+  //set up the transmission/reception of messages to occur at 500Hz (2mS) timer interrupt
+  Timer3.attachInterrupt(PrintScreen).setFrequency(1).start();
+       
+  //start CAN ports,  enable interrupts and RX masks, set the baud rate here
+  CANport0.initialize(_500K);
+}
+
+
+UINT8 i;
+UINT32 maxTime;
+
+void loop()
+{
+CANport0.run(POLLING); 
+}
+
+//this is our timer interrupt handler, called at XmS interval
+void PrintScreen()
+{
+  float a;
+  String bbb;
+  char result[20];
+
+  //print out our latest OBDII data
+  a = OBD_Speed.getData();
+  bbb = String(a,0); 
+  bbb.toCharArray(result,20);
+  Serial.write("Speed=");
+  Serial.write(result);
+  Serial.write("\n");
+
+
+  
+  a = OBD_EngineSpeed.getData();
+  bbb = String(a,0); 
+  bbb.toCharArray(result,20);
+  Serial.write("Engine RPM=");
+  Serial.write(result);
+  Serial.write("\n");
+
+
+  a = OBD_Coolant.getData();
+  bbb = String(a,0); 
+  bbb.toCharArray(result,20);
+  Serial.write("Coolant Temperature=");
+  Serial.write(result);
+  Serial.write("\n");
+
+
+  a = OBD_EngineLoad.getData();
+  bbb = String(a,0); 
+  bbb.toCharArray(result,20);
+  Serial.write("Engine Load=");
+  Serial.write(result);
+  Serial.write("\n");
+
+  Serial.write(";");
+   
+}
---
+++ connyct_macchina_websocket_demo.ino
@@ -0,0 +1,167 @@
+/*
+ * 
+ * Author: Sushant Mainaly
+ * Company: Connyct
+ * Website: https://www.connyct.com
+ * 
+ * License: Beerware - If you use it and you meet me someday buy me beer!
+ * If I have used external libraries, the licence above does not apply to them.
+ * 
+ * Date: May 1st 2017
+ */
+ 
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h>
+#include <Hash.h>
+
+/*
+ * THE FOLLOWING CONSTANTS WILL HAVE TO BE REPLACED IN ORDER TO USE THIS CODE
+ */
+#define SSID_NAME           "NAME HERE" // Wifi Name
+#define SSID_PASSWORD       "Password Here" // Wifi Password
+/* Token and secret key from https://www.connyct.com */
+#define CONNYCT_TOKEN       "xxxxxxxxxxxxxx"
+#define CONNYCT_SECRET_KEY  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ /*
+  * END OF CONSTANTS TO CHANGE
+  */
+
+#define CONNYCT_URL       "connyct.com"
+#define WS_PORT           80
+#define CONNYCT_WS_LOC    "/api/v1/w/"
+
+#define USE_SERIAL Serial
+#define INCOMING_BUFFER_LENGTH 1024
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+boolean connected = false;
+
+
+int incomingDataIndex = 0;
+String incomingData = "";
+boolean completeDataReceived = false;
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+    switch(type) {
+        case WStype_DISCONNECTED:
+            connected = false;
+            USE_SERIAL.printf("[WSc] Disconnected: %s\n", payload);
+            break;
+        case WStype_CONNECTED:
+            {
+                USE_SERIAL.printf("[WSc] Connected to url: %s\n",  payload);
+                // send credentials as first message when connected
+                webSocket.sendTXT("{\"token\":\""+String(CONNYCT_TOKEN)+"\",\"secret_key\":\""+String(CONNYCT_SECRET_KEY)+"\"}");
+            }
+            connected = true;
+            break;
+        case WStype_TEXT:
+            USE_SERIAL.printf("[WSc] received: %s\n", payload);
+            break;
+        case WStype_BIN:
+            USE_SERIAL.printf("[WSc] received binary length: %u\n", length);
+            hexdump(payload, length);
+            break;
+    }
+}
+
+void setup() {
+    USE_SERIAL.begin(115200);
+    USE_SERIAL.setDebugOutput(true);
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    incomingData.reserve(INCOMING_BUFFER_LENGTH);
+    
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    // WiFi Name and Password
+    WiFiMulti.addAP(SSID_NAME, SSID_PASSWORD);
+    
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    //connyctserver
+    webSocket.begin(CONNYCT_URL,WS_PORT,CONNYCT_WS_LOC);
+    webSocket.onEvent(webSocketEvent);
+}
+
+
+void parseAndSendData()
+{
+  // convert string in buffer to json and send it
+  // find index of = 
+  int i = 0;
+  String dataToSend = "{\n\"";
+  int equalsIndex = 0;
+  int endIndex = 0;
+
+  for (i=0;i<INCOMING_BUFFER_LENGTH;i++)
+  {
+    if( incomingData[i] == ';')
+    {
+      dataToSend += "}";
+      break;
+    }
+
+    if( incomingData[i] == '\n')
+    {
+      if (incomingData[i+1] != ';')
+      {
+        dataToSend+= "\",\n\"";
+      }
+      else
+      {
+        dataToSend+= "\"\n";
+      }
+    }
+    else if ( incomingData[i] == '=')
+    {
+      dataToSend+= "\":\"";
+    }
+    else
+    {
+      dataToSend+=incomingData[i];
+    }
+  }
+  Serial.print(dataToSend);
+   webSocket.sendTXT(dataToSend);
+}
+
+
+void loop() {
+    webSocket.loop();
+
+      while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    incomingData +=inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == ';') 
+    {
+      completeDataReceived = true;
+    }
+  }
+
+  
+    if (completeDataReceived && connected)
+    {
+      // need to process data that we received which is in incomingData
+      parseAndSendData();
+      incomingData="";
+      completeDataReceived = false;
+    }     
+}
+
+
